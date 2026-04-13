d5a0d38071b5d5c5b9d5a17128c4e486b3cfe7a9
C.C <36757974+CeraCharlesCC@users.noreply.github.com>
2026-04-14 06:37:50 +0900

    stick JDK version

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,9 @@ tasks.withType<KotlinCompile> {
 }
 
 java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
     withSourcesJar()
